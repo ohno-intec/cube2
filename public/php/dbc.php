5e413cc4e8cebf0252165b41671cb1a3fb888abc
Takuya.O <webmaster@intec1998.co.jp>
2018-01-10 17:54:20 +0900

    revised dbc

--- public/php/dbc.php
+++ public/php/dbc.php
@@ -5,7 +5,7 @@ function DBC(){
     if($server_name == env("LOCALHOST")){
         return mysqli_connect(env("LOCALHOST"), env("LOCAL_DB_USER"), env("LOCAL_DB_PASSWORD") ,env("LOCAL_DB_NAME"));
     }elseif($server_name == env("HOST_NAME")){
-        return mysqli_connect(env("DB_HOST "), env("DB_USER"), env("DB_PASSWORD"), env("DB_NAME"));
+        return mysqli_connect(env("DB_HOST "), env("DB_USERNAME"), env("DB_PASSWORD"), env("DB_DATABASE"));
     }
 
 }
